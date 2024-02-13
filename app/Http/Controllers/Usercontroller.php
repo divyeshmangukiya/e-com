034ff6247b2ba113b12f106f440e42e7708ec216
Divyesh <yashcomputers@Divyesh-MacBook-Pro.local>
2024-02-13 11:10:45 +0530

    login done

---
+++ app/Http/Controllers/Usercontroller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Psy\CodeCleaner\ReturnTypePass;
+use Illuminate\Support\Facades\Hash;
+  
+
+class Usercontroller extends Controller
+{
+    function login(Request $req)
+    {
+        return $req->input();
+
+        $user = User::where(['email'=>$req->email])->first();
+
+
+        if(!$user || !Hash:: check($req->password,$user->password)){
+            return 'wronng';
+
+        }else{
+            $req->session()->put('user',$user);
+            return redirect('/');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Usercontroller;
+use App\Http\Controllers\Productcontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
+
+
+Route::post('/login',[Usercontroller::class,'login']);
+Route::get('/',[Productcontroller::class,'index']);
